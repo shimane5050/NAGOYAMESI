a26e79bcfcb5d53ce3a074939dad38a08977050e
shimane5050 <shimane5050@gmail.com>
2024-06-03 18:41:47 +0900

    店舗モデルに営業時間フィールド・定休日フィールドを追加

--- ngyproject/crud/models.py
+++ ngyproject/crud/models.py
@@ -7,8 +7,17 @@ class Shop(models.Model):
     place = models.CharField(max_length=20, verbose_name='住所')
     tel_number_regex = RegexValidator(regex=r'^[0-9]+$', message = ("Tel Number must be entered in the format: '09012345678'. Up to 15 digits allowed."))
     tel_number = models.CharField(validators=[tel_number_regex], max_length=15, default='114758758',verbose_name='電話番号')
+    open_time = models.TimeField(verbose_name="開店時間", default="09:00")
+    close_time = models.TimeField(verbose_name="閉店時間", default="22:00")
     description = models.CharField(max_length=200, verbose_name='詳細')
     img = models.ImageField(blank=True, default='takosan.png')
+    closed_monday = models.BooleanField(verbose_name="月曜定休", default=False)
+    closed_tuesday = models.BooleanField(verbose_name="火曜定休", default=False)
+    closed_wednesday = models.BooleanField(verbose_name="水曜定休", default=False)
+    closed_thursday = models.BooleanField(verbose_name="木曜定休", default=False)
+    closed_friday = models.BooleanField(verbose_name="金曜定休", default=False)
+    closed_saturday = models.BooleanField(verbose_name="土曜定休", default=False)
+    closed_sunday = models.BooleanField(verbose_name="日曜定休", default=False)
 
     def __str__(self):
         return self.name
