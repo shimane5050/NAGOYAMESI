15e77a6cef9a41cd758219a49e9e333321cb8279
shimane5050 <shimane5050@gmail.com>
2024-06-03 16:29:17 +0900

    Shopモデルの追加・ShopAdminの追加

--- ngyproject/crud/admin.py
+++ ngyproject/crud/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Shop
 
-# Register your models here.
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'place')
+    search_fields = ('name','name_kana')
+
+admin.site.register(Shop, ShopAdmin)
--- ngyproject/crud/models.py
+++ ngyproject/crud/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=20)
+    name_kana = models.CharField(max_length=20)
+    place = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
