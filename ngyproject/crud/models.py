2ec25e0e7c702fbd94ccd01be923a678c938fd76
shimane5050 <shimane5050@gmail.com>
2024-06-03 18:27:56 +0900

    店舗モデルに電話番号フィールドを追加

--- ngyproject/crud/models.py
+++ ngyproject/crud/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Shop(models.Model):
     name = models.CharField(max_length=20, verbose_name='店名')
     name_kana = models.CharField(max_length=20, verbose_name='店名かな')
     place = models.CharField(max_length=20, verbose_name='住所')
+    tel_number_regex = RegexValidator(regex=r'^[0-9]+$', message = ("Tel Number must be entered in the format: '09012345678'. Up to 15 digits allowed."))
+    tel_number = models.CharField(validators=[tel_number_regex], max_length=15, default='114758758',verbose_name='電話番号')
     description = models.CharField(max_length=200, verbose_name='詳細')
     img = models.ImageField(blank=True, default='takosan.png')
 
