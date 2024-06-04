0c4a08b24452a0264a22889b081dfba46c19e500
shimane5050 <shimane5050@gmail.com>
2024-06-04 15:15:50 +0900

    Templateフォルダの作成・top.html追加

--- ngyproject/ngyproject/urls.py
+++ ngyproject/ngyproject/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from crud import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.TopView.as_view(), name='top'),
 ]
 
 if settings.DEBUG:
