0107ca333f31a46b7c336861d9015335308d2cf6
shimane5050 <shimane5050@gmail.com>
2024-06-04 09:04:23 +0900

    エリアモデル・フィールドの追加・verbosenameの追加

--- ngyproject/crud/admin.py
+++ ngyproject/crud/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from .models import Shop, Category
+from .models import Shop, Category, Area
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
 
 class ShopAdmin(admin.ModelAdmin):
     list_display = ('name', 'place')
     search_fields = ('name','name_kana',)
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-
-admin.site.register(Shop, ShopAdmin)
+admin.site.register(Area, AreaAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Shop, ShopAdmin)
 
 # Django管理サイト名変更
 admin.site.site_header = 'NAGOYAMESI管理サイト'
--- ngyproject/crud/models.py
+++ ngyproject/crud/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
+# エリアモデル
+class Area(models.Model):
+     name = models.CharField(max_length=20, verbose_name='エリア名')
+ 
+     def __str__(self):
+         return self.name
+     
+     class Meta:
+        verbose_name = 'エリア'
+        verbose_name_plural = 'エリア'
+
+
 # カテゴリモデル
 class Category(models.Model):
-     name = models.CharField(max_length=20)
+     name = models.CharField(max_length=20, verbose_name='カテゴリ名')
  
      def __str__(self):
          return self.name
@@ -23,7 +35,7 @@ class Shop(models.Model):
     open_time = models.TimeField(verbose_name="開店時間", default="09:00")
     close_time = models.TimeField(verbose_name="閉店時間", default="22:00")
     description = models.CharField(max_length=200, verbose_name='詳細')
-    img = models.ImageField(blank=True, default='takosan.png')
+    img = models.ImageField(blank=True, default='takosan.png', verbose_name='画像')
     closed_monday = models.BooleanField(verbose_name="月曜定休", default=False)
     closed_tuesday = models.BooleanField(verbose_name="火曜定休", default=False)
     closed_wednesday = models.BooleanField(verbose_name="水曜定休", default=False)
@@ -31,7 +43,9 @@ class Shop(models.Model):
     closed_friday = models.BooleanField(verbose_name="金曜定休", default=False)
     closed_saturday = models.BooleanField(verbose_name="土曜定休", default=False)
     closed_sunday = models.BooleanField(verbose_name="日曜定休", default=False)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='カテゴリ')
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True, verbose_name='エリア')
+
 
     def __str__(self):
         return self.name
